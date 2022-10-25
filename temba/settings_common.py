fbbae6890683762072baa3707b1acb39c6e8a526
Teeh Amaral <contato@teehamaral.me>
2022-10-25 12:05:00 -0300

    Remove mGage from this release

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -1287,7 +1287,7 @@ CHANNEL_TYPES = [
     "temba.channels.types.discord.DiscordType",
     "temba.channels.types.webchat.WebChatType",
     "temba.channels.types.rocketchat.RocketChatType",
-    "temba.channels.types.mgage.MGageType",
+    # "temba.channels.types.mgage.MGageType",
 ]
 
 # set of ISO-639-3 codes of languages to allow in addition to all ISO-639-1 languages
