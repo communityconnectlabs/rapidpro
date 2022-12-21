a4fe9470895dc347dd62db18aff13237e091e49e
Teeh Amaral <contato@teehamaral.me>
2022-12-21 09:53:20 -0300

    Revert changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,12 +5,12 @@ case $1 in
     app)
         python3.9 manage.py compress --extension=.haml --force
         python3.9 docker/clear-compressor-cache.py
-#        python3.9 manage.py migrate --noinput
+        python3.9 manage.py migrate --noinput
         /usr/local/bin/supervisord -n -c docker/supervisor-app.conf
     ;;
     celery)
         python3.9 docker/clear-compressor-cache.py
-#        python3.9 manage.py migrate --noinput
+        python3.9 manage.py migrate --noinput
         /usr/local/bin/supervisord -n -c docker/supervisor-celery.conf
     ;;
 
