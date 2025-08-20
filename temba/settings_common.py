179354340fd957f50087ff173c8cb0c7d917ba25
Ihor Palii <palii.ihor@uvik.net>
2025-08-20 17:45:11 +0300

    Changed configuration to allow iframing same origin pages

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -1530,6 +1530,7 @@ GOOGLE_FONT_API_KEY = os.environ.get("GOOGLE_FONT_API_KEY", "")
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = ["GET"]
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 # Contacts import via excel
 # if set to True will not raise error on duplicate, instead will use last row
